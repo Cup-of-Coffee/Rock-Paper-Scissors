a80b8f0092e72e0b0ccb34283f6edbd1cbbd8166
Cup-of-Coffee <larry.le124@gmail.com>
2023-01-20 11:04:59 -0500

    getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,20 @@
 
-function getComputerChoice {
+function getComputerChoice(){
+    let computerChoice = Math.floor(Math.random() * 3);
+    let result;
+
+    if(computerChoice = 1){
+        result = "Rock";
+    }else if(computerChoice = 2){
+        result = "Paper";
+    }else{
+        result = "Scissors";
+    }
+
+    return result;
+}
+
+function name(params) {
     
 }
 
